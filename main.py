6b750aad8016913bf5ac1521feefbe30a924974b
ogorovoy <83642161+ogorovoy@users.noreply.github.com>
2023-06-05 15:14:58 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import shutil
+import hashlib
+
+my_dir= "C:/Git/control_summe/"
+
+def mask_funkction(my_dir):
+    for file in os.listdir(my_dir):
+        if os.path.isfile(os.path.join(my_dir, file)):
+
+            create_new_folder = os.path.splitext(file)[0]
+            os.makedirs(os.path.join(my_dir, create_new_folder), exist_ok=True)
+
+            old_path = os.path.join(my_dir, file)
+            new_path = os.path.join(my_dir, create_new_folder, file)
+            shutil.copy2(old_path, new_path)
+
+            txt_file_name = os.path.splitext(file)[0] + ".txt"
+            txt_file_path = os.path.join(my_dir, create_new_folder, txt_file_name)
+            with open(old_path, 'rb') as file:
+                file_content = file.read()
+                control_summe = hashlib.md5(file_content).hexdigest()
+
+            with open(txt_file_path, 'w') as txt_file:
+                txt_file.write(f"control_summe: {control_summe}")
+
+mask_funkction(my_dir)
